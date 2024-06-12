ace5bff7d8614d9864bd6819992511b4e70bc666
Brian McGaw <bmcgaw91@gmail.com>
2024-06-11 23:06:12 -0500

    Finish navbars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+// Toggle nav icon color
+const icon = document.getElementsByClassName("navbar-toggler-icon")[0];
 
+icon.addEventListener("click", () => {
+  icon.classList.toggle("active-icon");
+});
