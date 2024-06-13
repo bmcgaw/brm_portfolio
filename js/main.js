19f2f114faf3b4eeb85bdc9ee35a6e2cc627a7ef
Brian McGaw <bmcgaw91@gmail.com>
2024-06-12 23:30:45 -0500

    Finish home page and navigation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const icon = document.getElementsByClassName("navbar-toggler-icon")[0];
 icon.addEventListener("click", () => {
   icon.classList.toggle("active-icon");
 });
+
+// Slide in home page content
+document.addEventListener("DOMContentLoaded", () => {
+  const profileImage = document.querySelector(".profile-image");
+  const homePageContent = document.querySelector(".home-content");
+
+  homePageContent.classList.add("slide-in");
+  profileImage.classList.add("slide-in");
+});
