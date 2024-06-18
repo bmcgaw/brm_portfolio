115cd6db4759074689d4f2e53fca04d2d9a952c3
Brian McGaw <bmcgaw91@gmail.com>
2024-06-17 23:40:40 -0500

    Finish main structure of site and add styling to sections

--- js/main.js
+++ js/main.js
@@ -5,15 +5,7 @@ icon.addEventListener("click", () => {
   icon.classList.toggle("active-toggler-icon");
 });
 
-// Slide in home page content
-document.addEventListener("DOMContentLoaded", () => {
-  const profileImage = document.querySelector(".profile-image");
-  const homePageContent = document.querySelector(".home-content");
-
-  homePageContent.classList.add("slide-in");
-  profileImage.classList.add("slide-in");
-});
-
+// Toggle dropdown visibility for top navbar
 const navbar = document.querySelector("#navbarNav");
 
 navbar.addEventListener("click", () => {
@@ -22,3 +14,29 @@ navbar.addEventListener("click", () => {
   navbar.classList.toggle("show");
   navActive.classList.remove("active-toggler-icon");
 });
+
+// Change color on vertical nav links when active
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(
+  ".bi-house-door-fill,.bi-tools,.bi-folder-fill,.bi-globe,.bi-book-fill,.bi-envelope-fill"
+);
+
+function handleScroll() {
+  const currentSection = Array.from(sections).find((section) => {
+    const rect = section.getBoundingClientRect();
+    return (
+      rect.top <= window.innerHeight / 2 &&
+      rect.bottom >= window.innerHeight / 2
+    );
+  });
+
+  navLinks.forEach((navLink) => {
+    if (navLink.classList.contains(`${currentSection.id}`)) {
+      navLink.classList.add("active-vertical-icon");
+    } else {
+      navLink.classList.remove("active-vertical-icon");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
